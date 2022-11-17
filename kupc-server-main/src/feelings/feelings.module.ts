2a7d2adf02c2534ff26663abef7b68e5b4890c41
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2022-11-17 10:26:09 +0500

    delete

--- kupc-server-main/src/feelings/feelings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FeelingsController } from './feelings.controller';
-import { FeelingsService } from './feelings.service';
-
-@Module({
-  controllers: [FeelingsController],
-  providers: [FeelingsService]
-})
-export class FeelingsModule {}
